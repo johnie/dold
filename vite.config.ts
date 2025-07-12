b5a9052922b2ff4449d414ca724fa725a92edec8
Johnie Hjelm <johnie@hjelm.im>
2025-07-12 22:27:06 +0200

    feat: add Tailwind CSS plugin to Vite configuration; update alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { cloudflare } from '@cloudflare/vite-plugin';
 import { defineConfig } from 'vite';
 import ssrPlugin from 'vite-ssr-components/plugin';
+import tailwindcss from '@tailwindcss/vite';
+
 import path from 'path';
 
 export default defineConfig({
-  plugins: [cloudflare(), ssrPlugin()],
+  plugins: [cloudflare(), ssrPlugin(), tailwindcss()],
   resolve: {
     alias: {
-      '@/*': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
