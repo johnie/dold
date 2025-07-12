80f862e48e5a79c863e4ac3db757d8788b89e493
Johnie Hjelm <johnie@hjelm.im>
2025-07-12 23:17:01 +0200

    fix: update renderer to use reactRenderer and enhance HTML structure with meta tags

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,14 +1,23 @@
-import { jsxRenderer } from 'hono/jsx-renderer'
-import { Link, ViteClient } from 'vite-ssr-components/hono'
+import { reactRenderer } from '@hono/react-renderer';
+import {
+  Script,
+  Link,
+  ViteClient,
+  ReactRefresh,
+} from 'vite-ssr-components/react';
 
-export const renderer = jsxRenderer(({ children }) => {
+export const renderer = reactRenderer(({ children }) => {
   return (
-    <html>
+    <html lang="en" className="dark">
       <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <ViteClient />
+        <ReactRefresh />
         <Link href="/src/style.css" rel="stylesheet" />
+        <Script src="/src/client/index.tsx" />
       </head>
       <body>{children}</body>
     </html>
-  )
-})
+  );
+});
