ad136b0e3c7d4fb871712081298a2743563d19e5
Johnie Hjelm <johnie@hjelm.im>
2025-07-12 21:04:31 +0200

    feat: implement initial server setup with Hono framework

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import { Hono } from 'hono';
+import { renderer } from './renderer';
+import { cors } from 'hono/cors';
+import router from './routes';
+import type { DoldApp } from './types';
+
+const app = new Hono<DoldApp>();
+
+app.use('*', cors());
+
+app.use(renderer);
+
+app.get('/', (c) => {
+  return c.render(<h1>Hello!</h1>);
+});
+
+const apiRoutes = app.route('/api', router);
+
+export type RouteType = typeof apiRoutes;
+
+export default app;
