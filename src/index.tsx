16a777f8a045fce999489f7f4f1e2be4f0f6e279
Johnie Hjelm <johnie@hjelm.im>
2025-07-13 20:41:06 +0200

    feat: add title and description to home page rendering

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { renderer } from '@/renderer';
 import { cors } from 'hono/cors';
 import router from '@/routes';
 import type { DoldApp } from '@/types';
+import { titleTemplate } from './lib/utils';
 
 const app = new Hono<DoldApp>();
 
@@ -11,7 +12,10 @@ app.use('*', cors());
 app.use(renderer);
 
 app.get('/', (c) => {
-  return c.render(<div id="root"></div>);
+  return c.render(<div id="root"></div>, {
+    title: titleTemplate('Home'),
+    description: 'Welcome to Dold, your secure message encryption service.',
+  });
 });
 
 const apiRoutes = app.route('/api', router);
