332fab455bfc1cf6d059404a8628b5d342a30fdc
Johnie Hjelm <johnie@hjelm.im>
2025-07-13 20:41:23 +0200

    feat: add Toaster component to root rendering

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,8 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import App from './app';
+import { Toaster } from '@/components/ui/sonner';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = createRoot(rootElement);
-  root.render(<App />);
+  root.render(
+    <>
+      <Toaster />
+      <App />
+    </>
+  );
 }
