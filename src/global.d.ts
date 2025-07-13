af88a67d97caf6abb552d84e4a0df6310d101a92
Johnie Hjelm <johnie@hjelm.im>
2025-07-13 20:40:56 +0200

    feat: add type declarations for Hono context renderer and React renderer props

---
+++ src/global.d.ts
@@ -0,0 +1,14 @@
+import {} from 'hono';
+import type { Meta } from '@/types';
+
+declare module 'hono' {
+  interface ContextRenderer {
+    (content: string | Promise<string>, props: Meta):
+      | Response
+      | Promise<Response>;
+  }
+}
+
+declare module '@hono/react-renderer' {
+  interface Props extends Meta {}
+}
